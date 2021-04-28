7b252ded663810b7b4f2738245dd09018fa014a8
warapetch <warapetch.rpvs@gmail.com>
2021-04-28 12:41:29 +0700

    เพิ่มไดอะลอกวัน+เวลา

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
 import 'package:learnform1/widgets/checkbox_form_field.dart';
 
 void main() {
-
     runApp(MyApp());
 
 }
@@ -14,6 +15,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
       return MaterialApp(
+
+          // แสดงปฏิทินภาษาไทย  แต่ไม่เป็น พ.ศ. !!
+          // 2. add GlobalMaterialLocalizations
+            // localizationsDelegates: [
+            //         GlobalMaterialLocalizations.delegate
+            //         ],
+            // supportedLocales: [
+            //     const Locale('en'),
+            //     const Locale('th')
+            //     ],
+
                 title: "ลงทะเบียน",
                 home: HomePage(),
       );
@@ -33,6 +45,10 @@ class _HomePageState extends State<HomePage> {
     final ctrlName = new TextEditingController();
     final ctrlSurName = new TextEditingController();
     var grpGender = 'male';
+    DateTime selectedDate = DateTime.now();
+    int thisYear = 0;
+    TimeOfDay selectedTime = TimeOfDay.now();
+
     String email = "";
     bool bLicenseCar = false, bLicenseBike = false;
     bool bSoldier = false, bMarry = false;
@@ -45,11 +61,74 @@ class _HomePageState extends State<HomePage> {
 
 
     @override
+    void initState() {      // OnCreate
+      super.initState();
+
+        setState(() {
+            thisYear = DateTime.now().year;
+           selectedDate = DateTime.now();
+        });
+    }
+
+    // Method showDlgDatePicker
+    Future<void> showDlgDatePicker(BuildContext context) async {
+        final DateTime ? picked = await showDatePicker(
+            context: context,
+            cancelText: "ยกเลิก",
+            confirmText: "เลือก",
+            helpText: "เลือกวันที่",
+            //locale : const Locale("th","TH"),   // ต้องผ่าน 2 ขั้นตอนก่อน 1.add package pubspec 2.add GlobalMaterialLocalizations
+            initialDate: selectedDate,
+            firstDate: DateTime(thisYear),    // ตามที่ต้องการกำหนด
+            lastDate: DateTime(thisYear+1),   // ห่างจากวันแรก +1 ปี หรือตามต้องการ
+            initialDatePickerMode: DatePickerMode.day,
+            );
+
+            if (picked != null && picked != selectedDate)
+                setState(() {
+                    selectedDate = picked;
+                    print("เลือกวันที่ : $selectedDate");
+                });
+    }
+
+
+    // Method showDlgTimePicker
+    Future<void> showDlgTimePicker(BuildContext context) async {
+
+        final TimeOfDay ? picked = await showTimePicker(
+            context: context,
+            cancelText: "ยกเลิก",
+            confirmText: "เลือก",
+            helpText: "เลือกเวลา",
+            initialTime: selectedTime,
+            initialEntryMode: TimePickerEntryMode.dial,
+            //initialEntryMode: TimePickerEntryMode.input,
+
+            // Display 24 hours //
+            // builder: (BuildContext context,Widget child) {
+            //          return  MediaQuery(
+            //                  data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+            //                  child: child
+            //                  );
+            //              },
+            );
+
+            if (picked != null && picked != selectedTime)
+                setState(() {
+                    selectedTime = picked;
+                    print("เลือกเวลา : $selectedTime");
+                });
+    }
+
+
+    @override
     Widget build(BuildContext context) {
 
         return
         Scaffold(
-            appBar: AppBar(title: Text("ลงทะเบียน"),),
+            appBar: AppBar(title: Text("ลงทะเบียน"),
+                ),
+
             body: SingleChildScrollView(
                     child: Container(
                                 padding: EdgeInsets.all(10),
@@ -61,6 +140,23 @@ class _HomePageState extends State<HomePage> {
                                             buildSwitch(),
                                             buildSlider(),
                                             Text("อายุ : ${iAge.round()} ปี"),
+                        //---Date ----------------------------------------------------------
+                                            ElevatedButton(
+                                                child: Text("เลือก วันที่"),
+                                                onPressed: (){
+                                                     showDlgDatePicker(context);
+                                                    }
+                                            ),
+                                            Text("เลือกวันที่ : ${selectedDate.day}/${selectedDate.month}/${selectedDate.year+543}"),
+                        //---Time ----------------------------------------------------------
+                                            ElevatedButton(
+                                                child: Text("เลือก เวลา"),
+                                                onPressed: (){
+                                                     showDlgTimePicker(context);
+                                                    }
+                                            ),
+                                            Text("เลือกเวลา : "+cnvTime12To24(selectedTime.format(context))),
+
                                             buildDropDown(),
                                             buildForm(),
                                             ElevatedButton(
@@ -84,6 +180,14 @@ class _HomePageState extends State<HomePage> {
 
 
   }
+
+String cnvTime12To24(String timeValue){
+
+    var dtFormat = DateFormat("h:mm a");
+    var dtTime = dtFormat.parse(timeValue);
+    return DateFormat('HH:mm').format(dtTime);
+
+}
 
 
     Widget buildForm(){
@@ -128,14 +232,8 @@ class _HomePageState extends State<HomePage> {
     Widget buildDropDown(){
 
         return DropdownButton(
-                value: channel,  // value must in List
-                items: channels,
-                // [
-                //     DropdownMenuItem(
-                //             child: Text("Facebook"),
-                //             value: "Facebook",
-                //             ),
-                // ]
+                value: channel,     // value (must in List items)
+                items: channels,    // list items
                 onChanged: (selectedValue){
                     setState(() {
                         channel = selectedValue.toString();
@@ -144,6 +242,31 @@ class _HomePageState extends State<HomePage> {
         );
 
     }
+
+
+    // Widget buildDate(BuildContext context) {
+
+    //     return  ElevatedButton(
+    //                     child: Text("Date"),
+    //                     onPressed: (){
+    //                         // pickupDate(context);
+    //                             showDatePicker(
+    //                                     context: context,
+    //                                     initialDate: choosedateTime,
+    //                                     firstDate: DateTime(thisYear),
+    //                                     lastDate: DateTime(thisYear)
+    //                                     ).then((date) {
+    //                                         setState(() {
+    //                                             //date ??= DateTime.now();
+    //                                             choosedateTime = date ??= DateTime.now();
+    //                                         });
+    //                                     });
+    //                     }
+
+    //             );
+    // }
+
+
 
     Widget buildSlider(){
 
